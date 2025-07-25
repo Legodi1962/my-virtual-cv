1623ca126d2b8682e69f3987feaec672d565d76c
Legodi Mahlatse <112449455+Legodi1962@users.noreply.github.com>
2025-07-25 18:44:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Show welcome alert when the page loads
-window.onload = function () {
-  alert("Welcome to Lesiba Simon Mahlatse Legodi's Portfolio!");
-};
-
-// Automatically update the year in the footer
-document.addEventListener("DOMContentLoaded", function () {
-  const yearSpan = document.getElementById("current-year");
-  if (yearSpan) {
-    yearSpan.textContent = new Date().getFullYear();
-  }
-});
+function showMessage() {
+  document.getElementById("message").innerText = "Welcome to my Virtual CV!";
+}
